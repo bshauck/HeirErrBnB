ab93e53531b06a074cd883cdecd7cebb08e3a662
bshauck <williamrshauck@gmail.com>
2023-09-01 22:07:38 -0600

    added Booking model and table

---
+++ backend/db/migrations/20230902035257-create-booking.js
@@ -0,0 +1,53 @@
+'use strict';
+const options = {};
+options.tableName = 'Bookings';
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+};
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'Spots'},
+        onDelete: 'CASCADE'
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {model: 'Users'},
+        onDelete: 'CASCADE'
+      },
+      startDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      endDate: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable(options);
+  }
+};
---
+++ backend/db/models/booking.js
@@ -0,0 +1,49 @@
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Booking extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Booking.init({
+    spotId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    userId: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    startDate: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      validate: {
+        later(value) {
+          if (value <= Date.now())
+            throw new Error('Start date must be in the future.')
+        }
+      }
+    },
+    endDate: {
+      allowNull: false,
+      type: DataTypes.DATE,
+      validate: {
+        laterThan(value) {
+          if (this.startDate > value)
+            throw new Error('End date must be after start date.')
+        }
+      }
+    },
+}, {
+    sequelize,
+    modelName: 'Booking',
+  });
+  return Booking;
+};
