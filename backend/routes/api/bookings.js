e3d8c8e2fb97197cc0a0935b46b4aa3cb758b764
bshauck <williamrshauck@gmail.com>
2024-03-02 19:10:33 -0700

    DRY duplicated function into new file

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -4,49 +4,9 @@ const { dayDate, ymd, ymdt } = require('../../utils/normalizeDate')
 const { Booking, Spot, SpotImage } = require('../../db/models');
 const { Op } = require('sequelize');
 const { adjustPojo } = require('../../utils/pojo')
+const {bookingOk} = require('./bookingOk')
+
 const router = require('express').Router();
-
-
-
-// DUPLICATED: refactor
-async function bookingOk(startDate, endDate, next, spotId, id) {
-    const specificErrText = "Sorry, this spot is already booked for the specified dates";
-    const err = Error("Part of date range is already booked");
-    startDate = dayDate(startDate);
-    endDate = dayDate(endDate);
-    const idClause = id === undefined
-        ? {} : {id: {[Op.ne]: id}};
-    const errors = {};
-    const conflict = await Booking.findOne({
-    where: {startDate: {[Op.lt]: ymd(endDate)},
-            endDate: {[Op.gt]: ymd(startDate)},
-                spotId: {[Op.eq]: spotId},
-                ...idClause}
-    });
-    if (conflict) {
-        const conflict2 = await Booking.findOne({
-            where: {startDate: {[Op.lte]: ymd(startDate)},
-                    endDate: {[Op.gt]: ymd(startDate)},
-                        spotId: {[Op.eq]: spotId},
-                        ...idClause}
-        });
-        if (conflict2) {errors.startDate = "Start date conflicts with an existing booking"; err.message = specificErrText;}
-        const conflict3 = await Booking.findOne({
-            where: {startDate: {[Op.lt]: ymd(endDate)},
-                    endDate: {[Op.gte]: ymd(endDate)},
-                    spotId: {[Op.eq]: spotId},
-                    ...idClause}
-        });
-        if (conflict3) {errors.endDate = "End date conflicts with an existing booking"; err.message = specificErrText;}
-    }
-    if (conflict) {
-        err.title = "Bad Request";
-        err.errors = errors;
-        return unauthor(next, err);
-    }
-    else return true;
-};
-
 
 // Return all of current user's bookings.
 router.get('/current', requireAuth, async (req, res) => {
